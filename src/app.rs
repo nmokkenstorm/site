6820ee49f91d7a70025ed969175d93c00f793685
Niels Mokkenstorm <niels@mokkenstorm.dev>
2023-08-19 21:40:12 +0200

    feat: add routing

--- src/app.rs
+++ src/app.rs
@@ -1,7 +1,24 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 use crate::pages::Home;
 use crate::partials::{Footer, Header};
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/")]
+    Home,
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! { <Home/>},
+        Route::NotFound => html! { <h1>{ "404" }</h1> },
+    }
+}
 
 #[function_component]
 pub fn App() -> Html {
@@ -9,7 +26,9 @@ pub fn App() -> Html {
       <div>
         <main>
             <Header/>
-            <Home />
+            <BrowserRouter>
+                <Switch<Route> render={switch} />
+            </BrowserRouter>
         </main>
         <Footer/>
       </div>
